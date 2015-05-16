3673502fbd691d4932553539d74408ffe30aa0c8
Banchon Wu <myboo.wu@gmail.com>
2015-05-16 12:05:51 +0800

    add file stream

--- src/game.cpp
+++ src/game.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <string>
 
 int main(int argc, char *argv[])
 {
@@ -17,7 +19,7 @@ int main(int argc, char *argv[])
 
 	sockaddr_in serverAddress;
 	sockaddr_in localAddress;
-	int pid; //player id;
+	//int pid; //player id;
 
 	::memset(&serverAddress, 0, sizeof(serverAddress));
 	::memset(&localAddress, 0, sizeof(localAddress));
@@ -38,7 +40,7 @@ int main(int argc, char *argv[])
 	}
 	localAddress.sin_port = ::htons(::atoi(argv[4]));
 
-	pid = ::atoi(argv[5]);
+	//pid = ::atoi(argv[5]);
 
 	//establish the TCP connect with the game server.
 	int localSocket = ::socket(AF_INET, SOCK_STREAM, 0);
@@ -59,6 +61,18 @@ int main(int argc, char *argv[])
 	//if connect to game server failed, just try to connect again.
 	while(::connect(localSocket, (sockaddr *)&serverAddress, sizeof(sockaddr_in)) == -1)
 		::usleep(100 * 1000); //sleep 100ms
+
+	FILE* localSocketStream = fdopen(localSocket, "r+");
+	if(localSocketStream == NULL) {
+		std::cout << "error: fdopen() fail." << std::endl;
+		return -1;
+	}
+
+	std::string regmsg = "reg: ";
+	regmsg.append(argv[5]);
+	regmsg.append(" boo \n");
+	std::cout << regmsg.c_str();
+
 
 	//TCP connection has already established.
 	
